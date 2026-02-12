70eaa1527e71f2e229d4e90aa34534bacb46a350
Garvit <garvitjr47@gmail.com>
2026-02-12 13:26:48 +0530

    Added Gradcae

---
+++ src/gradcam.py
@@ -0,0 +1,52 @@
+import torch
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+from torchvision import transforms
+from model import FightDetector
+
+
+class GradCAM:
+    def __init__(self, model, target_layer):
+        self.model = model
+        self.target_layer = target_layer
+
+        self.gradients = None
+        self.activations = None
+
+        self.target_layer.register_forward_hook(self.save_activation)
+        self.target_layer.register_backward_hook(self.save_gradient)
+
+    def save_activation(self, module, input, output):
+        self.activations = output
+
+    def save_gradient(self, module, grad_input, grad_output):
+        self.gradients = grad_output[0]
+
+    def generate(self, input_tensor, class_idx):
+        output = self.model(input_tensor)
+        self.model.zero_grad()
+
+        loss = output[:, class_idx]
+        loss.backward()
+
+        gradients = self.gradients[0]
+        activations = self.activations[0]
+
+        weights = torch.mean(gradients, dim=(1, 2))
+        cam = torch.zeros(activations.shape[1:], dtype=torch.float32)
+
+        for i, w in enumerate(weights):
+            cam += w * activations[i]
+
+        cam = torch.relu(cam)
+        cam = cam / torch.max(cam)
+
+        return cam.detach().cpu().numpy()
+
+
+def overlay_cam_on_image(img, cam):
+    cam = cv2.resize(cam, (img.shape[1], img.shape[0]))
+    heatmap = cv2.applyColorMap(np.uint8(255 * cam), cv2.COLORMAP_JET)
+    overlay = heatmap * 0.4 + img
+    return np.uint8(overlay)
